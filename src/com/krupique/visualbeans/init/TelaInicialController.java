4d425399cb45d2b77913d20990e3d3469776989a
krupc <krupc@DESKTOP-411F316>
2019-02-14 00:26:21 -0200

    Consigo criar vários tabs de maneira dinâmica; Adicionei os ícones (não mexi no CSS);

--- src/com/krupique/visualbeans/init/TelaInicialController.java
+++ src/com/krupique/visualbeans/init/TelaInicialController.java
@@ -6,8 +6,21 @@
 package com.krupique.visualbeans.init;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +29,145 @@ import javafx.fxml.Initializable;
  */
 public class TelaInicialController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ImageView imgnew;
+    @FXML
+    private ImageView imgopen;
+    @FXML
+    private ImageView imgsave;
+    @FXML
+    private ImageView imgcompile;
+    @FXML
+    private TabPane tabAnalyzes;
+    @FXML
+    private TabPane tabFiles;
+    
+    private ArrayList<Tab> listtabs;
+    @FXML
+    private Pane panenew;
+    @FXML
+    private Pane paneopen;
+    @FXML
+    private Pane panesave;
+    @FXML
+    private Pane panecompile;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        carragarImagens(1);
+        
+        listtabs = new ArrayList<Tab>();
     }    
+
+    public void carragarImagens(int num)
+    {
+        double escala = 0.8;
+        
+        imgnew.setScaleX(escala);
+        imgnew.setScaleY(escala);
+        
+        imgopen.setScaleX(escala);
+        imgopen.setScaleY(escala);
+        
+        imgsave.setScaleX(escala);
+        imgsave.setScaleY(escala);
+        
+        imgcompile.setScaleX(escala);
+        imgcompile.setScaleY(escala);
+        
+        Image img = new Image("com/krupique/visualbeans/resources/new" + num + ".png");
+        imgnew.setImage(img);
+        img = new Image("com/krupique/visualbeans/resources/open" + num +".png");
+        imgopen.setImage(img);
+        img = new Image("com/krupique/visualbeans/resources/save" + num +".png");
+        imgsave.setImage(img);
+        img = new Image("com/krupique/visualbeans/resources/play" + num + ".png");
+        imgcompile.setImage(img);
+    }
     
+    @FXML
+    private void newFile(ActionEvent event) {
+        Tab tab = new Tab("Sem título" + (listtabs.size() + 1));
+        listtabs.add(tab);
+        
+        CodeArea code = new CodeArea();
+        
+        AnchorPane pane = new AnchorPane();
+        tab.setContent(pane);
+        pane.getChildren().add(configurarCodeArea(code));
+        
+        tabFiles.getTabs().add(tab);
+    }
+
+    public CodeArea configurarCodeArea(CodeArea code)
+    {
+        code.setPrefSize(1070, 336);
+        code.setLayoutX(5);
+        code.setLayoutY(5);
+        code.setParagraphGraphicFactory(LineNumberFactory.get(code));
+        return code;
+    }
+    
+    @FXML
+    private void openFile(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeFile(ActionEvent event) {
+        tabFiles.getTabs().remove(tabFiles.getSelectionModel().getSelectedItem());
+        
+    }
+
+    @FXML
+    private void saveFile(ActionEvent event) {
+        tabFiles.getSelectionModel().getSelectedItem();
+    }
+
+    @FXML
+    private void saveAs(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitProgram(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtCompileExited(MouseEvent event) {
+        panecompile.setStyle("-fx-background-color:white");
+    }
+
+    @FXML
+    private void evtCompileEntered(MouseEvent event) {
+        panecompile.setStyle("-fx-background-color:black");
+    }
+
+    @FXML
+    private void evtNewExited(MouseEvent event) {
+        panenew.setStyle("-fx-background-color:white");
+    }
+
+    @FXML
+    private void evtNewEntered(MouseEvent event) {
+        panenew.setStyle("-fx-background-color:black");
+    }
+
+    @FXML
+    private void evtOpenExited(MouseEvent event) {
+        paneopen.setStyle("-fx-background-color:white");
+    }
+
+    @FXML
+    private void evtOpenEntered(MouseEvent event) {
+        paneopen.setStyle("-fx-background-color:black");
+    }
+
+    @FXML
+    private void evtSaveExited(MouseEvent event) {
+        panesave.setStyle("-fx-background-color:white");
+    }
+
+    @FXML
+    private void evtSaveEntered(MouseEvent event) {
+        panesave.setStyle("-fx-background-color:black");
+    }
 }
