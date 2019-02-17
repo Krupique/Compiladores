f4a083476c406193469357eef78c2ae43966b0d5
krupc <krupc@DESKTOP-411F316>
2019-02-17 11:51:17 -0300

    Preciso fazer em tempo de execução para ir identificando os erros. Parte da Análise Léxica. Toda a parte de Análise Sintática.

---
+++ src/com/krupique/visualbeans/analysis/Lexica.java
@@ -0,0 +1,9 @@
+package com.krupique.visualbeans.analysis;
+
+/**
+ *
+ * @author Henrique K. Secchi
+ */
+public class Lexica {
+    
+}
--- src/com/krupique/visualbeans/utils/Colors.java
+++ src/com/krupique/visualbeans/utils/Colors.java
@@ -17,33 +17,30 @@ public class Colors {
     private String exampleString = "This is a WARNING for an INFO! Please stay tuned";
 
     private static final String[] KEYWORDS = new String[]{"and", "boolean", "catch", "char", "double", "else", "false", "for", "if", "int", "mentira", "new", "null", "nulo", "NULL", "or", "program", "string", "true", "try", "var", "verdade", "while"};
-    //private static final String[] SIMBOLOS = new String[]{"azar"};
-
-    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    //private static final String[] SIMBOLOS = new String[]{"a, b, c, d, e, f"};
+    
     //private static final String SIMBOLOS_PATTERN = "\\b(" + String.join("|", SIMBOLOS) + ")\\b";
-    private static final String LETTERS_PATTERN = "[A-Za-z@#`$.]+";
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String LETTERS_PATTERN = "[A-Za-z#`$._]+";
+    private static final String SIMBOLOS_PATTERN = "\\{|\\}|\\[|\\]|\\(|\\)|\\%|\\;|\\<|\\>|\\*|\\=|\\?|\\:|\\||\\&|\\+|\\-|\\/|\\~|\\!|\\^";
+    private static final String NUMBERS_PATTERN = "[0-9]+";
+    private static final String CHAR_PATTERN = "\'[\\S+\\s]{1}\'";
+    private static final String STRING_PATTERN = "\".*{0,}\"";
+    private static final String COMENT_PATTERN = "@.*{0,}@";
+    private static final String RESTO_PATTERN = "\\S+";
     
-    private static final String BRACE_PATTERN = "\\{|\\}";
-    private static final String BRACKET_PATTERN = "\\[|\\]";
-    private static final String PARENTESES_PATTERN = "\\(|\\)";
-    private static final String SEMICOLON_PATTERN = "\\;";
-    private static final String SIMBOLOS_PATTERN = "[=?:|&^+-/~!]+";
-    //Falta os de números e alguns caracteres.
-    //Preciso fazer em tempo de execução para ir identificando os erros.
-    //Falta fazer o pattern de comentários também.
-    //Dar um jeito de arrumar o cursor piscante (tá me irritando demais essa buceta).
-    
-    private static final String STRING_PATTERN = "\"([^\"]|\\\")*\"";
     private static final Pattern PATTERN = Pattern.compile(
-            "(?<KEYWORD>" + KEYWORD_PATTERN + ")" + 
-            "|(?<SIMBOLO>" + SIMBOLOS_PATTERN + ")" +
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")" +
             "|(?<LETTERS>" + LETTERS_PATTERN + ")" +
+            "|(?<SIMBOLOS>" + SIMBOLOS_PATTERN + ")" +
+            "|(?<NUMBERS>" + NUMBERS_PATTERN + ")" +
+            "|(?<CHAR>" + CHAR_PATTERN + ")" +
             "|(?<STRING>" + STRING_PATTERN + ")" +
-            "|(?<BRACE>" + BRACE_PATTERN + ")" +
-            "|(?<BRACKET>" + BRACKET_PATTERN + ")" +
-            "|(?<PAREN>" + PARENTESES_PATTERN + ")" +
-            "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+            "|(?<COMENT>" + COMENT_PATTERN + ")" +
+            "|(?<RESTO>" + RESTO_PATTERN + ")"
     );
+    //Preciso fazer em tempo de execução para ir identificando os erros.
+    //Dar um jeito de arrumar o cursor piscante (tá me irritando demais essa buceta).
 
     public Colors(CodeArea codeArea) {
         this.codeArea = codeArea;
@@ -56,34 +53,34 @@ public class Colors {
     
     public void start() {
         String css = "com/krupique/visualbeans/styles/main_code_area.css";
-        codeArea.textProperty().addListener((obs, oldText, newText) -> {
+        codeArea.textProperty().addListener((obs, oldText, newText) -> 
+        {
             codeArea.setStyleSpans(0, computeHighlighting(newText));
         });
         codeArea.replaceText(0, 0, exampleString);
         codeArea.getStylesheets().add(css);
     }
     
-    private static StyleSpans<Collection<String>> computeHighlighting(
-            String text) {
+    private static StyleSpans<Collection<String>> computeHighlighting( String text)
+    {
         Matcher matcher = PATTERN.matcher(text);
         int lastKwEnd = 0;
         StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
-        while (matcher.find()) {
+        while (matcher.find()) 
+        {
             String styleClass = 
-                    matcher.group("KEYWORD") != null ? "keyword": 
-                    matcher.group("SIMBOLO") != null ? "simbolo" : 
-                    matcher.group("LETTERS") != null ? "letters" : 
-                    matcher.group("BRACE") != null ? "simbolo" : 
-                    matcher.group("BRACKET") != null ? "simbolo" : 
-                    matcher.group("SEMICOLON") != null ? "simbolo" : 
-                    matcher.group("PAREN") != null ? "simbolo" : 
-                    matcher.group("STRING") != null ? "string" : null;
-            /* never happens */
+                matcher.group("KEYWORD") != null ? "keyword": 
+                matcher.group("LETTERS") != null ? "letters" :  
+                matcher.group("SIMBOLOS") != null ? "simbolo" : 
+                matcher.group("NUMBERS") != null ? "numeros" :
+                matcher.group("CHAR") != null ? "char" :
+                matcher.group("STRING") != null ? "string" :
+                matcher.group("COMENT") != null ? "coment" :
+                matcher.group("RESTO") != null ? "resto" : null;
+            
             assert styleClass != null;
-            spansBuilder.add(Collections.emptyList(), matcher.start()
-                    - lastKwEnd);
-            spansBuilder.add(Collections.singleton(styleClass), matcher.end()
-                    - matcher.start());
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
             lastKwEnd = matcher.end();
         }
         spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
